2436b47f23437b743607da13fdae24b4d171dc69
fancyaction <labcoatdisaster@gmail.com>
2018-07-21 16:59:17 -0500

    Add storeController to index

--- helpers.js
+++ helpers.js
@@ -1,7 +1,3 @@
-/*
-  This is a file of data and helper functions that we can expose and use in our templating function
-*/
-
 // FS is a built in module to node that let's us read files from the system we're running on
 const fs = require('fs');
 
@@ -9,21 +5,25 @@ const fs = require('fs');
 exports.moment = require('moment');
 
 // Dump is a handy debugging function we can use to sort of "console.log" our data
-exports.dump = (obj) => JSON.stringify(obj, null, 2);
+exports.dump = obj => JSON.stringify(obj, null, 2);
 
 // Making a static map is really long - this is a handy helper function to make one
-exports.staticMap = ([lng, lat]) => `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=800x150&key=${process.env.MAP_KEY}&markers=${lat},${lng}&scale=2`;
+exports.staticMap = ([lng, lat]) =>
+  `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=800x150&key=${
+    process.env.MAP_KEY
+  }&markers=${lat},${lng}&scale=2`;
 
 // inserting an SVG
-exports.icon = (name) => fs.readFileSync(`./public/images/icons/${name}.svg`);
+exports.icon = name => fs.readFileSync(`./public/images/icons/${name}.svg`);
 
 // Some details about the site
 exports.siteName = `Now That's Delicious!`;
 
+// stores navbar items
 exports.menu = [
-  { slug: '/stores', title: 'Stores', icon: 'store', },
-  { slug: '/tags', title: 'Tags', icon: 'tag', },
-  { slug: '/top', title: 'Top', icon: 'top', },
-  { slug: '/add', title: 'Add', icon: 'add', },
-  { slug: '/map', title: 'Map', icon: 'map', },
+  { slug: '/stores', title: 'Stores', icon: 'store' },
+  { slug: '/tags', title: 'Tags', icon: 'tag' },
+  { slug: '/top', title: 'Top', icon: 'top' },
+  { slug: '/add', title: 'Add', icon: 'add' },
+  { slug: '/map', title: 'Map', icon: 'map' }
 ];
