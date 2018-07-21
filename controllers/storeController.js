c2a88f33c35c654a7fb5b31c749a0478065d30ab
fancyaction <labcoatdisaster@gmail.com>
2018-07-21 17:35:33 -0500

    Add 2nd middleware to index

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,12 @@
+exports.myMiddleware = (req, res, next) => {
+  req.name = 'Joy';
+  if (req.name === 'Joy') {
+    throw Error('That name is already taken.');
+  }
+  next();
+};
+
 exports.homePage = (req, res) => {
+  console.log(req.name);
   res.render('index');
 };
