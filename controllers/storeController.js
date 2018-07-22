3b7cce9ff65703fa313b2e91eaccfa5f4561e416
fancyaction <labcoatdisaster@gmail.com>
2018-07-22 18:39:41 -0500

    Add edit and updateStore controllers

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -15,7 +15,7 @@ exports.createStore = async (req, res) => {
     'success',
     `Successfully created ${store.name}. Care to leave a review?`
   );
-  res.redirect(`/store/${store.slug}`);
+  res.redirect(`/stores/${store.slug}`);
 };
 
 exports.getStores = async (req, res) => {
@@ -23,4 +23,28 @@ exports.getStores = async (req, res) => {
   const stores = await Store.find();
   // 2. Pass data to template
   res.render('stores', { title: 'Stores', stores });
+};
+
+exports.editStore = async (req, res) => {
+  // 1. Locate store from id
+  const store = await Store.findOne({ _id: req.params.id });
+  // 2. confirm store owner
+  // 3. Render edit form so user can update
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  // 1. locate and update store
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true,
+    runValidators: true
+  }).exec();
+  // 2. Flash success msg and redirect to store page
+  req.flash(
+    'success',
+    `Successfully updated <strong>${store.name}</strong>. <a href='/stores/${
+      store.slug
+    }'>View Store ➡</a>`
+  );
+  res.redirect(`/stores/${store.id}/edit`);
 };
