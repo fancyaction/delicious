7def6a7000875abc84f6d10f100cb2bface17fd5
fancyaction <labcoatdisaster@gmail.com>
2018-07-28 19:50:13 -0500

    Add login to register router.post

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 const { catchErrors } = require('../handlers/errorHandlers');
 
@@ -34,9 +35,11 @@ router.post(
 router.get('/login', userController.loginForm);
 router.get('/register', userController.registerForm);
 
-// 1. Validate registration data
-// 2. Register user
-// 3. Log-in user
-router.post('/register', userController.validateRegister);
+router.post(
+  '/register',
+  userController.validateRegister,
+  userController.register,
+  authController.login
+);
 
 module.exports = router;
